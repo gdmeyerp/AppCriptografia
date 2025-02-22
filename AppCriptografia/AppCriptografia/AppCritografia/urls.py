80d7e746eac521cc2b8fc8f431957edb6dde6caf
gdmeyerp <gdmeyerp@unal.edu.co>
2025-02-22 15:46:04 -0500

    Correccion link de descarga MIDI

--- AppCriptografia/AppCriptografia/AppCritografia/urls.py
+++ AppCriptografia/AppCriptografia/AppCritografia/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),  # Usa namespace correctamente
@@ -19,3 +20,6 @@ urlpatterns = [
     path('cifrado_musical/', include('cifrado_musical.urls')),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
