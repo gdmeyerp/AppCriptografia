60de7d3ad57d32f55d68dc748f92606cd37dded9
Juan A. Rodríguez <84048520+jrodriguezru@users.noreply.github.com>
2025-02-25 09:00:33 -0500

    Arreglos varios para el server

--- AppCriptografia/AppCriptografia/aes/apps.py
+++ AppCriptografia/AppCriptografia/aes/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+import os
 
 
 class AesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "aes"
+    path = os.path.dirname(os.path.abspath(__file__))
--- AppCriptografia/AppCriptografia/eliptica/apps.py
+++ AppCriptografia/AppCriptografia/eliptica/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+import os
 
 
 class ElipticaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'eliptica'
+    path = os.path.dirname(os.path.abspath(__file__))
